47ad7a2bdb3b4fb98a6cb5ccd0d2c19216d2ddd5
igor <igor.240340@gmail.com>
2024-10-28 18:54:12 +0700

    Implement Triangle
    In progress.

---
+++ Triangle/main.cpp
@@ -0,0 +1,60 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr int width = 800;
+constexpr int height = 600;
+
+class TriangleApplication {
+public:
+    void run() {
+        init_window();
+        init_vulkan();
+        main_loop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    void init_window() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+    }
+
+    void init_vulkan() {
+
+    }
+
+    void main_loop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    TriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
