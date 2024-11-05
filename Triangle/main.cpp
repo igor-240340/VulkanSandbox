a859f1deb3b5a53cad29fad90ff3141036188f65
igor <igor.240340@gmail.com>
2024-11-05 18:04:38 +0700

    Add image view

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -64,6 +64,7 @@ private:
     std::vector<VkImage> swap_chain_images;
     VkFormat swap_chain_image_format;
     VkExtent2D swap_chain_extent;
+    std::vector<VkImageView> swap_chain_image_views;
 
     void init_window() {
         glfwInit();
@@ -80,6 +81,7 @@ private:
         pick_physical_device();
         create_logical_device();
         create_swap_chain();
+        create_image_views();
     }
 
     void create_instance() {
@@ -203,6 +205,31 @@ private:
 
         swap_chain_image_format = surface_format.format;
         swap_chain_extent = extent;
+    }
+
+    void create_image_views() {
+        swap_chain_image_views.resize(swap_chain_images.size());
+
+        for (size_t i = 0; i < swap_chain_images.size(); i++) {
+            VkImageViewCreateInfo create_info{};
+            create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            create_info.image = swap_chain_images[i];
+            create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            create_info.format = swap_chain_image_format;
+            create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            create_info.subresourceRange.baseMipLevel = 0;
+            create_info.subresourceRange.levelCount = 1;
+            create_info.subresourceRange.baseArrayLayer = 0;
+            create_info.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(logical_device, &create_info, nullptr, &swap_chain_image_views[i]) != VK_SUCCESS) {
+                throw std::runtime_error("vk::failed to create image views!");
+            }
+        }
     }
 
     void create_surface() {
@@ -437,6 +464,10 @@ private:
     }
 
     void cleanup() {
+        for (const auto& image_view : swap_chain_image_views) {
+            vkDestroyImageView(logical_device, image_view, nullptr);
+        }
+
         vkDestroySwapchainKHR(logical_device, swap_chain, nullptr);
         vkDestroyDevice(logical_device, nullptr);
         vkDestroySurfaceKHR(instance, surface, nullptr);
