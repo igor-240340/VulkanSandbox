dbb8baf58bc9666a737ee304bdede80f90a59466
igor <igor.240340@gmail.com>
2025-06-16 05:54:37 +0700

    Clean

--- ModelLoading/main.cpp
+++ ModelLoading/main.cpp
@@ -108,7 +108,6 @@ struct Vertex {
         attribute_descriptions[0].binding = 0;
         attribute_descriptions[0].location = 0;
         attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
         attribute_descriptions[0].offset = offsetof(Vertex, pos);
 
         attribute_descriptions[1].binding = 0;
